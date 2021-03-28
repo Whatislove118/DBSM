eedde656512730c917961f9d243d922356588277
whatislove118 <oleg.pochikalin@mail.ru>
2021-03-28 17:09:11 +0300

    add_database

---
+++ Lab2/create_tbs.sql
@@ -0,0 +1,16 @@
+CREATE TABLESPACE BIG_GOLD_LAB
+DATAFILE '/u01/DB197/lastdata/node03/biggoldlab01.dbf' SIZE 10M,
+'/u01/DB197/lastdata/node03/biggoldlab02.dbf' SIZE 10M,
+'/u01/DB197/lastdata/node04/biggoldlab03.dbf' SIZE 10M,
+'/u01/DB197/lastdata/node04/biggoldlab04.dbf' SIZE 10M,
+'/u01/DB197/lastdata/node02/biggoldlab05.dbf' SIZE 10M;
+
+CREATE TABLESPACE BEST_ORANGE_WOOD
+DATAFILE '/u01/DB197/lastdata/node02/bestorangewood01.dbf' SIZE 10M,
+'/u01/DB197/lastdata/node03/bestorangewood02.dbf' SIZE 10M,
+'/u01/DB197/lastdata/node02/bestorangewood03.dbf' SIZE 10M;
+
+CREATE TABLESPACE COOL_GOLD_USER
+DATAFILE '/u01/DB197/lastdata/node03/coolgolduser01.dbf' SIZE 10M,
+'/u01/DB197/lastdata/node03/coolgolduser02.dbf' SIZE 10M,
+'/u01/DB197/lastdata/node01/coolgolduser03.dbf' SIZE 10M;
